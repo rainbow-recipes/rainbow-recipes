e8a4fbe078d40f9e7cdd7ff40aeb1f3e5ca499d4
jysc <jysc@hawaii.edu>
2025-11-20 12:42:07 -1000

    Remove table

--- src/app/my-store/page.tsx
+++ src/app/my-store/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth';
-import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
 import { vendorProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
