172f49bec8a5df8e98a65c42b0cef578acb61db8
jysc <jysc@hawaii.edu>
2025-12-03 00:44:58 -1000

    Fix vendors card img

--- src/components/vendors/VendorCard.tsx
+++ src/components/vendors/VendorCard.tsx
@@ -8,10 +8,20 @@ export default function VendorCard({ store }: { store: Store }) {
   return (
     <Card className="h-100 text-start shadow-sm border-0" style={{ width: '12rem' }}>
       {(store.image === null) && (
-      <Card.Img variant="top" src={defaultImage.src} alt={`${store.name}`} />
+      <Card.Img
+        variant="top"
+        src={defaultImage.src}
+        alt={`${store.name}`}
+        style={{ width: '12rem', height: '12rem', objectFit: 'cover' }}
+      />
       )}
       {store.image !== null && (
-        <Card.Img variant="top" src={store.image} alt={`${store.name}`} />
+        <Card.Img
+          variant="top"
+          src={store.image}
+          alt={`${store.name}`}
+          style={{ width: '12rem', height: '12rem', objectFit: 'cover' }}
+        />
       )}
       <Card.Body>
         <Card.Title style={{ fontSize: '0.95rem', marginBottom: '0.125rem', lineHeight: 1 }}>{store.name}</Card.Title>
